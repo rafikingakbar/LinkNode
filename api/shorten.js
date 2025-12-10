5bc93cc70cb319dc80171d7c514bc481fbb49c78
Rafi King Akbar <rafikingakbar@gmail.com>
2025-12-11 00:28:11 +0700

    use short/s/:code url

--- api/shorten.js
+++ api/shorten.js
@@ -33,7 +33,7 @@ module.exports = async (req, res) => {
 
     const shortCode = generateShortCode();
 
-    const shortUrl = `${req.headers.origin}/api/s/${shortCode}`;
+    const shortUrl = `${req.headers.origin}/s/${shortCode}`;
 
     const data = {
       originalUrl,
